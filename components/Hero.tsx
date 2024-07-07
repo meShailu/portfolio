4d149065e8e879ed5a707fcc10538ccd44947d3a
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-07 12:42:41 +0200

    spotlight created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,13 +10,10 @@ const Hero = () => {
           fill="white"
         />
         <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-scren"
-          fill="white"
+          className="top-10 -left-full h-[80vh] w-[50vw]"
+          fill="purple"
         />
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-scren"
-          fill="white"
-        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
     </div>
   );
