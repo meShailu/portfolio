4804d50665b6a4ae16e37dc3204e1b8660f1d219
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-08 10:49:25 +0200

    react-lottie intsalled for animation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -125,6 +125,12 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={``}></div>
+            </div>
+          )}
         </div>
       </div>
     </div>
