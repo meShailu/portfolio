fc9794e7198b99c08f8e203f7612894b0d2b3d64
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-12 13:05:34 +0200

    moving border component created from acernity

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,14 +1,33 @@
+import { workExperience } from "@/data";
 import React from "react";
+import { Button } from "./ui/MovingBorder";
 
 const Experience = () => {
   return (
     <div id="testimonials" className="py-20">
       <h1 className="heading">
         My
-        <span className="text-purple"> Work Experience</span>
+        <span className="text-purple"> work experience</span>
       </h1>
 
-      <div className="flex flex-col items-center max-lg:mt-10"></div>
+      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map((card) => (
+          <Button
+            key={card.id}
+            duration={Math.floor(Math.random() * 10000) + 10000}
+            borderRadius="1.75rem"
+            className="flex-1 text-white border-neutral-200 dark:border-slate-800"
+          >
+            <div>
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="lg:w-32 md:w-20 w-16"
+              />
+            </div>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 };
