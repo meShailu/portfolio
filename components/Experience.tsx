ada87808142dc43fb0b4e0494fcf856448258f5a
meShailu <123243572+meShailu@users.noreply.github.com>
2024-07-12 12:53:15 +0200

    experience component created

---
+++ components/Experience.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div id="testimonials" className="py-20">
+      <h1 className="heading">
+        My
+        <span className="text-purple"> Work Experience</span>
+      </h1>
+
+      <div className="flex flex-col items-center max-lg:mt-10"></div>
+    </div>
+  );
+};
+
+export default Experience;
